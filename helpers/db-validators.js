ec1600d21b758b61b34473727f6293df4cc2bed9
Administrator <admin@example.com>
2022-01-09 00:10:51 -0500

    avance ocultando pass y version del POST

---
+++ helpers/db-validators.js
@@ -0,0 +1,12 @@
+const Role = require('../models/rol')
+
+const esRoleValido = async (rol = '') => {
+    const existeRole = await Role.findOne({rol})
+    if(!existeRole){
+        throw new Error(`El rol ${rol} no existe en la BD`)
+    }   
+}
+
+module.exports = {
+    esRoleValido
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios')
+const { esRoleValido } = require('../helpers/db-validators')
 const { validarCampos } = require('../middlewares/validar-campos')
-const Role = require('../models/rol')
+
 
 const router = Router()
 
@@ -12,13 +13,7 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('correo', 'El correo no es válido').isEmail(),
     check('password', 'Password no válido').isLength({ min: 6 }),
-    // check('rol', 'Rol inválido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
-    check('rol').custom( async (rol = '') => {
-        const existeRole = await Role.findOne({rol})
-        if(!existeRole){
-            throw new Error(`El rol ${rol} no existe en la BD`)
-        }   
-    }),
+    check('rol').custom( esRoleValido ),
     validarCampos
 ], usuariosPost)
 
